d839d80950aa2dc8ca263df083a9d17c0067d41e
mohit <mohit878421@gmail.com>
2025-03-23 22:54:15 +0530

    implement routing

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,29 +1,22 @@
 "use client";
 import React from "react";
 import { Menu } from "antd";
-import { useRouter } from "next/navigation";
-import { menuItems } from "@/constants/menuItems";
+import { MENU_ITEMS } from "@/constants/menuItems";
 
-const Sidebar = () => {
-    const router = useRouter();
-
-    // Function to handle menu item click
-
-    return (
-        <>
-            <div style={{ height: 32, margin: 16, background: 'red' }} />
-            <Menu
-                theme="light"
-                defaultSelectedKeys={["5-1"]}
-                mode="inline"
-                items={menuItems}
-                style={{
-                    backgroundColor: "#4dafac",
-                    color: "#fff"
-                }}
-            />
-        </>
-    );
-};
+const Sidebar = () => (
+    <>
+        <div style={{ height: 32, margin: 16, background: 'red' }} />
+        <Menu
+            theme="light"
+            defaultSelectedKeys={["5-1"]}
+            mode="inline"
+            items={MENU_ITEMS}
+            style={{
+                backgroundColor: "#4dafac",
+                color: "#fff"
+            }}
+        />
+    </>
+);
 
 export default Sidebar;
--- src/constants/menuItems.tsx
+++ src/constants/menuItems.tsx
@@ -64,56 +64,56 @@ export const PATH_KEYS = {
 };
 
 
-export const menuItems = [
+export const MENU_ITEMS = [
     { key: PATH_KEYS.DASHBOARD, icon: getIcon("Dashboard"), label: <Link href={ROUTES.DASHBOARD} >Dashboard </Link>, path: ROUTES.DASHBOARD },
     { key: PATH_KEYS.MAILBOX, icon: getIcon("Mailbox"), label: <Link href={ROUTES.MAILBOX}>Mailbox</Link>, path: ROUTES.MAILBOX },
-    { key: PATH_KEYS.START_INTERVIEW, icon: getIcon("StartInterview"), label: 'Start Interview', path: ROUTES.START_INTERVIEW },
+    { key: PATH_KEYS.START_INTERVIEW, icon: getIcon("StartInterview"), label: <Link href={ROUTES.START_INTERVIEW}>Start Interview</Link>, path: ROUTES.START_INTERVIEW },
     {
         key: PATH_KEYS.TASK_MANAGER,
         icon: getIcon("TaskManager"),
-        label: 'Task Manager',
+        label: "Task Manager",
         children: [
-            { key: PATH_KEYS.OUTBOX, label: 'Outbox', path: ROUTES.OUTBOX },
-            { key: PATH_KEYS.APPOINTMENT_DAIRY, label: 'Appointment Dairy', path: ROUTES.APPOINTMENT_DAIRY },
-            { key: PATH_KEYS.DEPOSIT_BUCK, label: 'Deposit Buck', path: ROUTES.DEPOSIT_BUCK },
-            { key: PATH_KEYS.SMS_NON_RESPONDENTS, label: 'SMS Non-Respondents', path: ROUTES.SMS_NON_RESPONDENTS },
-            { key: PATH_KEYS.LONELY_HEARTS, label: 'Lonely Hearts', path: ROUTES.LONELY_HEARTS },
-            { key: PATH_KEYS.INCOMPLETE_PROFILE, label: 'Incomplete Profile', path: ROUTES.INCOMPLETE_PROFILE },
+            { key: PATH_KEYS.OUTBOX, label: <Link href={ROUTES.OUTBOX}>Outbox</Link>, path: ROUTES.OUTBOX },
+            { key: PATH_KEYS.APPOINTMENT_DAIRY, label: <Link href={ROUTES.APPOINTMENT_DAIRY}>Appointment Dairy</Link>, path: ROUTES.APPOINTMENT_DAIRY },
+            { key: PATH_KEYS.DEPOSIT_BUCK, label: <Link href={ROUTES.DEPOSIT_BUCK}>Deposit Buck</Link>, path: ROUTES.DEPOSIT_BUCK },
+            { key: PATH_KEYS.SMS_NON_RESPONDENTS, label: <Link href={ROUTES.SMS_NON_RESPONDENTS}>SMS Non-Respondents</Link>, path: ROUTES.SMS_NON_RESPONDENTS },
+            { key: PATH_KEYS.LONELY_HEARTS, label: <Link href={ROUTES.LONELY_HEARTS}>Lonely Hearts</Link>, path: ROUTES.LONELY_HEARTS },
+            { key: PATH_KEYS.INCOMPLETE_PROFILE, label: <Link href={ROUTES.INCOMPLETE_PROFILE}>Incomplete Profile</Link>, path: ROUTES.INCOMPLETE_PROFILE },
         ],
         path: ROUTES.TASK_MANAGER
     },
     {
         key: PATH_KEYS.CAMPAIGN_MANAGEMENT,
         icon: getIcon("CampaignManagement"),
-        label: 'Campaign Management',
+        label: "Campaign Management",
         children: [
             { key: PATH_KEYS.TELEMARKETING, label: <Link href={ROUTES.TELEMARKETING}>Telemarketing</Link>, path: ROUTES.TELEMARKETING },
-            { key: PATH_KEYS.ADVERTISEMENT, label: 'Advertisement', path: ROUTES.ADVERTISEMENT },
-            { key: PATH_KEYS.BUZZ_MANAGER, label: 'Buzz Manager', path: ROUTES.BUZZ_MANAGER },
-            { key: PATH_KEYS.SEND_SMS, label: 'Send SMS', path: ROUTES.SEND_SMS },
+            { key: PATH_KEYS.ADVERTISEMENT, label: <Link href={ROUTES.ADVERTISEMENT}>Advertisement</Link>, path: ROUTES.ADVERTISEMENT },
+            { key: PATH_KEYS.BUZZ_MANAGER, label: <Link href={ROUTES.BUZZ_MANAGER}>Buzz Manager</Link>, path: ROUTES.BUZZ_MANAGER },
+            { key: PATH_KEYS.SEND_SMS, label: <Link href={ROUTES.SEND_SMS}>Send SMS</Link>, path: ROUTES.SEND_SMS },
         ],
         path: ROUTES.CAMPAIGN_MANAGEMENT
     },
     {
         key: PATH_KEYS.CLIENT_MANAGEMENT,
         icon: getIcon("ClientManagement"),
-        label: 'Client Management',
+        label: "Client Management",
         children: [
-            { key: PATH_KEYS.VIEW_CLIENTS, label: 'View Clients', path: ROUTES.VIEW_CLIENTS },
-            { key: PATH_KEYS.INTRODUCTION, label: 'Introduction', path: ROUTES.INTRODUCTION },
-            { key: PATH_KEYS.PHOTOGRAPHER, label: 'Photographer', path: ROUTES.PHOTOGRAPHER },
+            { key: PATH_KEYS.VIEW_CLIENTS, label: <Link href={ROUTES.VIEW_CLIENTS}>View Clients</Link>, path: ROUTES.VIEW_CLIENTS },
+            { key: PATH_KEYS.INTRODUCTION, label: <Link href={ROUTES.INTRODUCTION}>Introduction</Link>, path: ROUTES.INTRODUCTION },
+            { key: PATH_KEYS.PHOTOGRAPHER, label: <Link href={ROUTES.PHOTOGRAPHER}>Photographer</Link>, path: ROUTES.PHOTOGRAPHER },
         ],
         path: ROUTES.CLIENT_MANAGEMENT
     },
     {
         key: PATH_KEYS.UTILITIES,
         icon: getIcon("Utilities"),
-        label: 'Utilities',
+        label: "Utilities",
         children: [
-            { key: PATH_KEYS.STORAGE, label: 'Storage', path: ROUTES.STORAGE },
-            { key: PATH_KEYS.RECEIPTS_MANAGER, label: 'Receipts Manager', path: ROUTES.RECEIPTS_MANAGER },
-            { key: PATH_KEYS.ISSUES_MANAGEMENT, label: 'Issues Management', path: ROUTES.ISSUES_MANAGEMENT },
-            { key: PATH_KEYS.NON_CONFORMANCE, label: 'Non Conformance', path: ROUTES.NON_CONFORMANCE },
+            { key: PATH_KEYS.STORAGE, label: <Link href={ROUTES.STORAGE}>Storage</Link>, path: ROUTES.STORAGE },
+            { key: PATH_KEYS.RECEIPTS_MANAGER, label: <Link href={ROUTES.RECEIPTS_MANAGER}>Receipts Manager</Link>, path: ROUTES.RECEIPTS_MANAGER },
+            { key: PATH_KEYS.ISSUES_MANAGEMENT, label: <Link href={ROUTES.ISSUES_MANAGEMENT}>Issues Management</Link>, path: ROUTES.ISSUES_MANAGEMENT },
+            { key: PATH_KEYS.NON_CONFORMANCE, label: <Link href={ROUTES.NON_CONFORMANCE}>Non Conformance</Link>, path: ROUTES.NON_CONFORMANCE },
         ],
         path: ROUTES.UTILITIES
     },
